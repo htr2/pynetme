8fefbeff731a64636de1baba6f3261a957c47ccb
htr2 <hvoelksen@gmail.com>
2018-08-21 22:46:15 +0100

    commit with lint

--- excersise5.py
+++ excersise5.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-"""Read the 'show_ip_bgp_summ.txt' file into your program. From this BGP output obtain the
-first and last lines of the output.
-From the first line use the string .split() method to obtain the local AS number.
-From the last line use the string .split() method to obtain the BGP peer IP address.
+"""Read the 'show_ip_bgp_summ.txt' file into your program.
+From this BGP output obtain the first and last lines of the output.
+From the first line use the string .split() method to obtain the local AS#
+From the last line use the string .split() method to obtain the BGP peer IP
 Print both local AS number and the BGP peer IP address to the screen.
 """
 from __future__ import print_function, unicode_literals
